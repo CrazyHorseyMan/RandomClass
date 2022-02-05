0cb448a27ecbf71170cf9c6993c7155927f6b532
CrazyHorseyMan <92572265+CrazyHorseyMan@users.noreply.github.com>
2022-02-05 10:59:52 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,419 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Timers.Timer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox9 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox10 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox11 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox12 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox13 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox14 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox15 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox16 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox17 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox18 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox19 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox20 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox21 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox22 = New System.Windows.Forms.CheckBox()
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(277, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Schüler auswählen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(19, 58)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox1.Size = New System.Drawing.Size(44, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Erik"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(19, 81)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(54, 17)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Jonas"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox3.Location = New System.Drawing.Point(19, 104)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(53, 17)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Haley"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Checked = True
+        Me.CheckBox4.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox4.Location = New System.Drawing.Point(19, 127)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(54, 17)
+        Me.CheckBox4.TabIndex = 4
+        Me.CheckBox4.Text = "David"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Checked = True
+        Me.CheckBox5.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox5.Location = New System.Drawing.Point(19, 150)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(46, 17)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "Max"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Checked = True
+        Me.CheckBox6.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox6.Location = New System.Drawing.Point(19, 173)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox6.TabIndex = 6
+        Me.CheckBox6.Text = "Simon"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Checked = True
+        Me.CheckBox7.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox7.Location = New System.Drawing.Point(19, 196)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(57, 17)
+        Me.CheckBox7.TabIndex = 7
+        Me.CheckBox7.Text = "Florian"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Checked = True
+        Me.CheckBox8.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox8.Location = New System.Drawing.Point(116, 58)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox8.TabIndex = 8
+        Me.CheckBox8.Text = "Solice"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1.0R
+        Me.Timer1.SynchronizingObject = Me
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(101, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 16)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Anwesend?"
+        '
+        'CheckBox9
+        '
+        Me.CheckBox9.AutoSize = True
+        Me.CheckBox9.Checked = True
+        Me.CheckBox9.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox9.Location = New System.Drawing.Point(227, 81)
+        Me.CheckBox9.Name = "CheckBox9"
+        Me.CheckBox9.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox9.Size = New System.Drawing.Size(50, 17)
+        Me.CheckBox9.TabIndex = 10
+        Me.CheckBox9.Text = "Leon"
+        Me.CheckBox9.UseVisualStyleBackColor = True
+        '
+        'CheckBox10
+        '
+        Me.CheckBox10.AutoSize = True
+        Me.CheckBox10.Checked = True
+        Me.CheckBox10.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox10.Location = New System.Drawing.Point(116, 81)
+        Me.CheckBox10.Name = "CheckBox10"
+        Me.CheckBox10.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox10.Size = New System.Drawing.Size(63, 17)
+        Me.CheckBox10.TabIndex = 11
+        Me.CheckBox10.Text = "Jennifer"
+        Me.CheckBox10.UseVisualStyleBackColor = True
+        '
+        'CheckBox11
+        '
+        Me.CheckBox11.AutoSize = True
+        Me.CheckBox11.Checked = True
+        Me.CheckBox11.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox11.Location = New System.Drawing.Point(116, 104)
+        Me.CheckBox11.Name = "CheckBox11"
+        Me.CheckBox11.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox11.Size = New System.Drawing.Size(53, 17)
+        Me.CheckBox11.TabIndex = 12
+        Me.CheckBox11.Text = "Anika"
+        Me.CheckBox11.UseVisualStyleBackColor = True
+        '
+        'CheckBox12
+        '
+        Me.CheckBox12.AutoSize = True
+        Me.CheckBox12.Checked = True
+        Me.CheckBox12.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox12.Location = New System.Drawing.Point(116, 127)
+        Me.CheckBox12.Name = "CheckBox12"
+        Me.CheckBox12.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox12.Size = New System.Drawing.Size(53, 17)
+        Me.CheckBox12.TabIndex = 13
+        Me.CheckBox12.Text = "Manu"
+        Me.CheckBox12.UseVisualStyleBackColor = True
+        '
+        'CheckBox13
+        '
+        Me.CheckBox13.AutoSize = True
+        Me.CheckBox13.Checked = True
+        Me.CheckBox13.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox13.Location = New System.Drawing.Point(116, 150)
+        Me.CheckBox13.Name = "CheckBox13"
+        Me.CheckBox13.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox13.Size = New System.Drawing.Size(50, 17)
+        Me.CheckBox13.TabIndex = 14
+        Me.CheckBox13.Text = "Lena"
+        Me.CheckBox13.UseVisualStyleBackColor = True
+        '
+        'CheckBox14
+        '
+        Me.CheckBox14.AutoSize = True
+        Me.CheckBox14.Checked = True
+        Me.CheckBox14.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox14.Location = New System.Drawing.Point(116, 173)
+        Me.CheckBox14.Name = "CheckBox14"
+        Me.CheckBox14.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox14.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox14.TabIndex = 15
+        Me.CheckBox14.Text = "Niklas"
+        Me.CheckBox14.UseVisualStyleBackColor = True
+        '
+        'CheckBox15
+        '
+        Me.CheckBox15.AutoSize = True
+        Me.CheckBox15.Checked = True
+        Me.CheckBox15.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox15.Location = New System.Drawing.Point(116, 196)
+        Me.CheckBox15.Name = "CheckBox15"
+        Me.CheckBox15.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox15.Size = New System.Drawing.Size(57, 17)
+        Me.CheckBox15.TabIndex = 16
+        Me.CheckBox15.Text = "Sönke"
+        Me.CheckBox15.UseVisualStyleBackColor = True
+        '
+        'CheckBox16
+        '
+        Me.CheckBox16.AutoSize = True
+        Me.CheckBox16.Checked = True
+        Me.CheckBox16.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox16.Location = New System.Drawing.Point(116, 219)
+        Me.CheckBox16.Name = "CheckBox16"
+        Me.CheckBox16.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox16.Size = New System.Drawing.Size(47, 17)
+        Me.CheckBox16.TabIndex = 17
+        Me.CheckBox16.Text = "Tom"
+        Me.CheckBox16.UseVisualStyleBackColor = True
+        '
+        'CheckBox17
+        '
+        Me.CheckBox17.AutoSize = True
+        Me.CheckBox17.Checked = True
+        Me.CheckBox17.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox17.Location = New System.Drawing.Point(227, 58)
+        Me.CheckBox17.Name = "CheckBox17"
+        Me.CheckBox17.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox17.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox17.TabIndex = 18
+        Me.CheckBox17.Text = "Emma"
+        Me.CheckBox17.UseVisualStyleBackColor = True
+        '
+        'CheckBox18
+        '
+        Me.CheckBox18.AutoSize = True
+        Me.CheckBox18.Checked = True
+        Me.CheckBox18.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox18.Location = New System.Drawing.Point(227, 104)
+        Me.CheckBox18.Name = "CheckBox18"
+        Me.CheckBox18.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox18.Size = New System.Drawing.Size(56, 17)
+        Me.CheckBox18.TabIndex = 19
+        Me.CheckBox18.Text = "Noelle"
+        Me.CheckBox18.UseVisualStyleBackColor = True
+        '
+        'CheckBox19
+        '
+        Me.CheckBox19.AutoSize = True
+        Me.CheckBox19.Checked = True
+        Me.CheckBox19.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox19.Location = New System.Drawing.Point(227, 127)
+        Me.CheckBox19.Name = "CheckBox19"
+        Me.CheckBox19.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox19.Size = New System.Drawing.Size(62, 17)
+        Me.CheckBox19.TabIndex = 20
+        Me.CheckBox19.Text = "Sabrina"
+        Me.CheckBox19.UseVisualStyleBackColor = True
+        '
+        'CheckBox20
+        '
+        Me.CheckBox20.AutoSize = True
+        Me.CheckBox20.Checked = True
+        Me.CheckBox20.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox20.Location = New System.Drawing.Point(227, 150)
+        Me.CheckBox20.Name = "CheckBox20"
+        Me.CheckBox20.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox20.Size = New System.Drawing.Size(54, 17)
+        Me.CheckBox20.TabIndex = 21
+        Me.CheckBox20.Text = "Farrell"
+        Me.CheckBox20.UseVisualStyleBackColor = True
+        '
+        'CheckBox21
+        '
+        Me.CheckBox21.AutoSize = True
+        Me.CheckBox21.Checked = True
+        Me.CheckBox21.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox21.Location = New System.Drawing.Point(227, 173)
+        Me.CheckBox21.Name = "CheckBox21"
+        Me.CheckBox21.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox21.Size = New System.Drawing.Size(72, 17)
+        Me.CheckBox21.TabIndex = 22
+        Me.CheckBox21.Text = "Anastasia"
+        Me.CheckBox21.UseVisualStyleBackColor = True
+        '
+        'CheckBox22
+        '
+        Me.CheckBox22.AutoSize = True
+        Me.CheckBox22.Checked = True
+        Me.CheckBox22.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox22.Location = New System.Drawing.Point(227, 196)
+        Me.CheckBox22.Name = "CheckBox22"
+        Me.CheckBox22.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox22.Size = New System.Drawing.Size(46, 17)
+        Me.CheckBox22.TabIndex = 23
+        Me.CheckBox22.Text = "Alex"
+        Me.CheckBox22.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(302, 239)
+        Me.Controls.Add(Me.CheckBox22)
+        Me.Controls.Add(Me.CheckBox21)
+        Me.Controls.Add(Me.CheckBox20)
+        Me.Controls.Add(Me.CheckBox19)
+        Me.Controls.Add(Me.CheckBox18)
+        Me.Controls.Add(Me.CheckBox17)
+        Me.Controls.Add(Me.CheckBox16)
+        Me.Controls.Add(Me.CheckBox15)
+        Me.Controls.Add(Me.CheckBox14)
+        Me.Controls.Add(Me.CheckBox13)
+        Me.Controls.Add(Me.CheckBox12)
+        Me.Controls.Add(Me.CheckBox11)
+        Me.Controls.Add(Me.CheckBox10)
+        Me.Controls.Add(Me.CheckBox9)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox8)
+        Me.Controls.Add(Me.CheckBox7)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox6 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox7 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox8 As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Timers.Timer
+    Friend WithEvents CheckBox22 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox21 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox20 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox19 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox18 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox17 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox16 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox15 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox14 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox13 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox12 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox11 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox10 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox9 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,247 @@
+﻿Public Class Form1
+    Dim checked(22), duration(22), i As Integer
+    Private Sub CheckBox1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If checked(1) = 1 Then
+            checked(1) = 0
+        Else
+            checked(1) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox7_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox7.CheckedChanged
+        If checked(7) = 1 Then
+            checked(7) = 0
+        Else
+            checked(7) = 1
+        End If
+    End Sub
+
+    Private Sub Timer1_Elapsed(sender As System.Object, e As System.Timers.ElapsedEventArgs) Handles Timer1.Elapsed
+        Dim z As Integer
+        For z = 1 To 22
+            checked(z) = 1
+        Next z
+        For i = 1 To 22
+            duration(i) = 2
+        Next
+        Timer1.Stop()
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If checked(2) = 1 Then
+            checked(2) = 0
+        Else
+            checked(2) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        If checked(3) = 1 Then
+            checked(3) = 0
+        Else
+            checked(3) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox4.CheckedChanged
+        If checked(4) = 1 Then
+            checked(4) = 0
+        Else
+            checked(4) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox5.CheckedChanged
+        If checked(5) = 1 Then
+            checked(5) = 0
+        Else
+            checked(5) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox6_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox6.CheckedChanged
+        If checked(6) = 1 Then
+            checked(6) = 0
+        Else
+            checked(6) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox8_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox8.CheckedChanged
+        If checked(8) = 1 Then
+            checked(8) = 0
+        Else
+            checked(8) = 1
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim student(22), count(22), j, death, problem, list As Integer
+        Dim namen(22) As String
+        list = -1
+        namen(1) = "Erik"
+        namen(2) = "Jonas"
+        namen(3) = "Haley"
+        namen(4) = "David"
+        namen(5) = "Max"
+        namen(6) = "Simon"
+        namen(7) = "Florian"
+        namen(8) = "Solice"
+        namen(9) = "Jennifer"
+        namen(10) = "Anika"
+        namen(11) = "Manu"
+        namen(12) = "Lena"
+        namen(13) = "Niklas"
+        namen(14) = "Sönke"
+        namen(15) = "Tom"
+        namen(16) = "Emma"
+        namen(17) = "Leon"
+        namen(18) = "Noelle"
+        namen(19) = "Sabrina"
+        namen(20) = "Farrell"
+        namen(21) = "Anastasia"
+        namen(22) = "Alex"
+        Randomize()
+        list = -list
+        Do
+            j = 0
+            For i = 1 To 22
+                If count(i) > 8 Then
+                    j = j + 1
+                End If
+            Next
+            problem = 0
+            death = Int(Rnd() * 22 + 1)
+            If checked(death) = 0 Then
+                problem = 1
+            End If
+            If duration(death) < 2 Then
+                problem = 1
+            End If
+            If list = 1 Then
+                If j > 1 Then
+                    If duration(death) < 9 Then
+                        problem = 1
+                    End If
+                End If
+            End If
+        Loop Until problem = 0
+        MsgBox(namen(death))
+        For i = 1 To 22
+            duration(i) = i + 1
+        Next
+        duration(death) = 0
+    End Sub
+
+    Private Sub CheckBox10_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox10.CheckedChanged
+        If checked(9) = 1 Then
+            checked(9) = 0
+        Else
+            checked(9) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox11_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox11.CheckedChanged
+        If checked(10) = 1 Then
+            checked(10) = 0
+        Else
+            checked(10) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox12_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox12.CheckedChanged
+        If checked(11) = 1 Then
+            checked(11) = 0
+        Else
+            checked(11) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox13_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox13.CheckedChanged
+        If checked(12) = 1 Then
+            checked(12) = 0
+        Else
+            checked(12) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox14_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox14.CheckedChanged
+        If checked(13) = 1 Then
+            checked(13) = 0
+        Else
+            checked(13) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox15_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox15.CheckedChanged
+        If checked(14) = 1 Then
+            checked(14) = 0
+        Else
+            checked(14) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox16_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox16.CheckedChanged
+        If checked(15) = 1 Then
+            checked(15) = 0
+        Else
+            checked(15) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox17_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox17.CheckedChanged
+        If checked(16) = 1 Then
+            checked(16) = 0
+        Else
+            checked(16) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox9_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox9.CheckedChanged
+        If checked(17) = 1 Then
+            checked(17) = 0
+        Else
+            checked(17) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox18_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox18.CheckedChanged
+        If checked(18) = 1 Then
+            checked(18) = 0
+        Else
+            checked(18) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox19_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox19.CheckedChanged
+        If checked(19) = 1 Then
+            checked(19) = 0
+        Else
+            checked(19) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox20_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox20.CheckedChanged
+        If checked(20) = 1 Then
+            checked(20) = 0
+        Else
+            checked(20) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox21_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox21.CheckedChanged
+        If checked(21) = 1 Then
+            checked(21) = 0
+        Else
+            checked(21) = 1
+        End If
+    End Sub
+
+    Private Sub CheckBox22_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox22.CheckedChanged
+        If checked(22) = 1 Then
+            checked(22) = 0
+        Else
+            checked(22) = 1
+        End If
+    End Sub
+End Class
